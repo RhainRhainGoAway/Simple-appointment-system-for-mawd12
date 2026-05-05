52b967bb5cdc1d09fc2545fe918cff7f4cbddd2d
Rhain Malvar <malvar.391122@santarosa.sti.edu.ph>
2026-05-05 16:43:18 +0800

    updates

--- api/obj/Debug/net9.0/AppointmentSystemAPI.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/AppointmentSystemAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppointmentSystemAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c257da781e16f506c68ea48eb09084f53afe080")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4fd426809511b77721e1e28c64876f131db660a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppointmentSystemAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppointmentSystemAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
